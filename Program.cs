40208590dd99092403681ad30cbaee4665f346b6
Philip Trubitsyn <trph@bk.ru>
2023-04-04 21:42:37 +0300

    Добавили все элементы содержащие 3 символа и меньше в новый массив и вывели в консоль

--- Program.cs
+++ Program.cs
@@ -6,12 +6,24 @@ for (int i = 0; i < n; i++)
     Console.Write($"Введите строку №{i + 1}:\n    ");
     startMass[i] = Console.ReadLine();
 }
-int length= 0;
-for (int i = 0; i < startMass.GetLength(0); i++){
-    if (startMass[i].Length < 4 ){
+int length = 0;
+for (int i = 0; i < startMass.GetLength(0); i++)
+{
+    if (startMass[i].Length < 4)
+    {
         length++;
     }
 }
 
-string [] newMass= new string [length];
+string[] newMass = new string[length];
 
+int count = 0;
+for (int i = 0; i < startMass.GetLength(0); i++)
+{
+    if (startMass[i].Length < 4)
+    {
+        newMass[count] = startMass[i];
+        count++;
+    }
+}
+Console.WriteLine(string.Join(",", newMass));
