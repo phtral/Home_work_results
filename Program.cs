2f3979db918681bb4a9e64120f1ab95bc2bd9a43
Philip Trubitsyn <trph@bk.ru>
2023-04-04 21:38:29 +0300

    Сделан ввод с клавиатуры и добавлена переменная Length

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите количество строк - ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] startMass = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите строку №{i + 1}:\n    ");
+    startMass[i] = Console.ReadLine();
+}
+int length= 0;
+for (int i = 0; i < startMass.GetLength(0); i++){
+    if (startMass[i].Length < 4 ){
+        length++;
+    }
+}
+
